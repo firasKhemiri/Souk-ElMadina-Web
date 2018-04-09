b1cb6df1be33ec8a7059a63f00020daa8305620a
rehab-zfr <rihab.zaafouri@esprit.tn>
2018-04-09 18:38:12 +0100

    delete items from panier

--- src/AppBundle/Controller/PanierController.php
+++ src/AppBundle/Controller/PanierController.php
@@ -40,114 +40,12 @@ class PanierController extends Controller
     }
 
     /**
-     * Creates a new panier entity.
-     *
-     * @Route("/new", name="panier_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-        $panier = new Panier();
-        $form = $this->createForm('AppBundle\Form\PanierType', $panier);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($panier);
-            $em->flush();
-
-            return $this->redirectToRoute('panier_show', array('id' => $panier->getId()));
-        }
-
-        return $this->render('@App/panier/new.html.twig', array(
-            'panier' => $panier,
-            'form' => $form->createView(),
-        ));
-    }
-
-    /**
-     * Finds and displays a panier entity.
-     *
-     * @Route("/{id}", name="panier_show")
-     * @Method("GET")
-     */
-    public function showAction(Panier $panier)
-    {
-        $deleteForm = $this->createDeleteForm($panier);
-
-        return $this->render('@App/panier/show.html.twig', array(
-            'panier' => $panier,
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Displays a form to edit an existing panier entity.
-     *
-     * @Route("/{id}/edit", name="panier_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Panier $panier)
-    {
-        $deleteForm = $this->createDeleteForm($panier);
-        $editForm = $this->createForm('AppBundle\Form\PanierType', $panier);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('panier_edit', array('id' => $panier->getId()));
-        }
-
-        return $this->render('@App/panier/edit.html.twig', array(
-            'panier' => $panier,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a panier entity.
-     *
-     * @Route("/{id}", name="panier_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Panier $panier)
-    {
-        $form = $this->createDeleteForm($panier);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($panier);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('panier_index');
-    }
-
-    /**
-     * Creates a form to delete a panier entity.
-     *
-     * @param Panier $panier The panier entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Panier $panier)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('panier_delete', array('id' => $panier->getId())))
-            ->setMethod('DELETE')
-            ->getForm();
-    }
-
-    /**
      * Creates a form to delete a panier entity.
      *
      * @Route("/addToPanier", name="addToPanier")
      * @Method("POST")
      */
-    public function addToPanier(Request $request)
+    public function addToPanierAction(Request $request)
     {
 
 
@@ -205,4 +103,46 @@ class PanierController extends Controller
     }
 
 
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("/panierItems",name="panierItems")
+     * @Method("GET")
+     */
+    public function getPanierItemAction(Request $request){
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $panier=$user->getPanier();
+        $s = $em->getRepository('AppBundle:Article')->getArticleFromPanier($panier);
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setCircularReferenceLimit(1);
+        // Add Circular reference handler
+        $normalizer->setCircularReferenceHandler(function ($object) {
+            return $object->getId();
+        });
+        $encoders = array(new JsonEncoder());
+        $normalizers = array($normalizer);
+        $serializer = new Serializer($normalizers, $encoders);
+
+        return new Response($serializer->serialize($s, 'json'));
+    }
+
+
+    /**
+     * @Route("/deleteItem",name="deleteItem")
+     * @Method("Post")
+     */
+    public function deleteItem(Request $request){
+
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+       $panier=$user->getPanier();
+        $ligne=$em->getRepository('AppBundle:Ligne')->findOneBy(
+            [
+                'panier'=>$panier,
+                'article'=>$em->getRepository('AppBundle:Article')->find($request->request->get('id'))]);
+        $em->remove($ligne);
+        $em->flush();
+        return new JsonResponse('success');
+    }
 }
